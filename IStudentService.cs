ca3423bda48f4c37c07f5bf6df8e3d692d003841
nhan nguyen <nhan nguyen@DESKTOP-QRDST9N>
2020-11-16 11:01:43 +0700

    Add project files.

---
+++ IStudentService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MSService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IStudentService" in both code and config file together.
+    [ServiceContract]
+    public interface IStudentService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/getStudentList"
+       )]
+        List<Student> getStudents();
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+           ResponseFormat = WebMessageFormat.Json,
+           RequestFormat = WebMessageFormat.Json,
+           BodyStyle = WebMessageBodyStyle.Bare,
+           UriTemplate = "api/v1/createStudent"
+      )]
+        bool createStudent(Student newStudent);
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+           ResponseFormat = WebMessageFormat.Json,
+           RequestFormat = WebMessageFormat.Json,
+           BodyStyle = WebMessageBodyStyle.Bare,
+           UriTemplate = "api/v1/editStudent/{id}"
+      )]
+        bool editStudent(string id, Student newStudent);
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+           ResponseFormat = WebMessageFormat.Json,
+           RequestFormat = WebMessageFormat.Json,
+           BodyStyle = WebMessageBodyStyle.Bare,
+           UriTemplate = "api/v1/deleteStudent/{id}"
+
+      )]
+        bool deleteStudent(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+          ResponseFormat = WebMessageFormat.Json,
+          RequestFormat = WebMessageFormat.Json,
+          BodyStyle = WebMessageBodyStyle.Bare,
+          UriTemplate = "api/v1/getClassList"
+     )]
+        List<Class> getClasses();
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+           ResponseFormat = WebMessageFormat.Json,
+           RequestFormat = WebMessageFormat.Json,
+           BodyStyle = WebMessageBodyStyle.Bare,
+           UriTemplate = "api/v1/createClass"
+      )]
+        bool createClass(Class newClass);
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+           ResponseFormat = WebMessageFormat.Json,
+           RequestFormat = WebMessageFormat.Json,
+           BodyStyle = WebMessageBodyStyle.Bare,
+           UriTemplate = "api/v1/editClass/{id}"
+      )]
+        bool editClass(string id, Class newClass);
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+           ResponseFormat = WebMessageFormat.Json,
+           RequestFormat = WebMessageFormat.Json,
+           BodyStyle = WebMessageBodyStyle.Bare,
+           UriTemplate = "api/v1/deleteClass/{id}"
+      )]
+        bool deleteClass(string id);
+    }
+}
---
+++ StudentService.svc.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MSService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "StudentService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select StudentService.svc or StudentService.svc.cs at the Solution Explorer and start debugging.
+    public class StudentService : IStudentService
+    {
+        MSStudentDataContext data = new MSStudentDataContext();
+
+        public bool createClass(Class newClass)
+        {
+            try
+            {
+                data.Classes.InsertOnSubmit(newClass);
+                data.SubmitChanges();
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool createStudent(Student newStudent)
+        {
+            try
+            {
+                data.Students.InsertOnSubmit(newStudent);
+                data.SubmitChanges();    
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool deleteClass(string id)
+        {
+            try
+            {
+                var classes = data.Classes.Where(c => c.ID == int.Parse(id)).FirstOrDefault();
+                data.Classes.DeleteOnSubmit(classes);
+                data.SubmitChanges();
+
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool deleteStudent(string id)
+        {
+            try
+            {
+                var student = data.Students.Where(c => c.ID == int.Parse(id)).FirstOrDefault();
+                data.Students.DeleteOnSubmit(student);
+                data.SubmitChanges();
+      
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool editClass(string id, Class newClass)
+        {
+            try
+            {
+                var class1 = data.Classes.Where(c => c.ID == int.Parse(id)).FirstOrDefault();
+                data.Classes.DeleteOnSubmit(class1);
+                data.Classes.InsertOnSubmit(newClass);
+                data.SubmitChanges();
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool editStudent(string id, Student newStudent)
+        {
+            try
+            {
+                var student = data.Students.Where(c => c.ID == int.Parse(id)).FirstOrDefault();
+                data.Students.DeleteOnSubmit(student);
+                data.Students.InsertOnSubmit(newStudent);
+                data.SubmitChanges();
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
+           
+        }
+
+        public List<Class> getClasses()
+        {
+
+            try
+            {
+                var class1 = (from Class in data.Classes select Class).ToList();
+
+                return class1;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public List<Student> getStudents()
+        {
+            try
+            {
+                var students = (from Student in data.Students select Student).ToList();
+
+                return students;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
